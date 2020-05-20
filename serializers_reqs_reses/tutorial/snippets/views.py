2eed3f18302fefd9911db1ea8fe3b215c92514b0
Greg <gsc229@gmail.com>
2020-05-20 02:39:55 -0400

    CRUD w/ mixins

--- serializers_reqs_reses/tutorial/snippets/views.py
+++ serializers_reqs_reses/tutorial/snippets/views.py
@@ -1,23 +1,37 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from django.http import Http404
-from rest_framework.response import Response 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
 """
 The root of our API is going to be a view that supports listing all the existing snippets, or creating a new snippet.
 """
+"""  
+helpful refresher on *args *kwargs:
+https://realpython.com/python-kwargs-and-args/
+"""
 
-class SnippetList(APIView):
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
   """
   List all code snippets, or create a new snippet.
   """
-  def get(self, request, format=None):
+  queryset = Snippet.objects.all()
+  serializer_class = SnippetSerializer
+  
+  """
+    BEFORE REFACTOR USING MIXINS
+    def get(self, request, format=None):
     snippets = Snippet.objects.all()
     serializer = SnippetSerializer(snippets, many=True)
     return Response(serializer.data)
+  """
+  def get(self, request, *args, **kwargs):    
+    return self.list(request, *args, **kwargs)
 
+  """ 
+  BEFORE REFACTOR USING MIXINS
   def post(self, request, format=None):
     
     serializer = SnippetSerializer(data=request.data)
@@ -26,42 +40,69 @@ class SnippetList(APIView):
       serializer.save()
       return Response(serializer.data)     
     return Response(serializer.errors)
+  """
+  def post(self, request, *args, **kwargs):    
+    return self.create(request, *args, **kwargs)
 
 
-
-class SnippetDetail(APIView):
+class SnippetDetail(mixins.RetrieveModelMixin, 
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin, 
+                    generics.GenericAPIView):
   """  
   Retrieve, update or delete a code snippet.
   """
   # class function to be called in all the single-instance methods
-  def get_object(self, pk):
-    try:    
-        return Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        raise Http404
+  """  
+    No LONGER NEED THIS BC of querset and serializer_class
+    def get_object(self, pk):
+      try:    
+          return Snippet.objects.get(pk=pk)
+      except Snippet.DoesNotExist:
+          raise Http404
+  """
+  queryset = Snippet.objects.all()
+  serializer_class = SnippetSerializer
 
-  def get(self, request, pk, format=None):
-    snippet = self.get_object(pk)
-    serializer = SnippetSerializer(snippet)
+  """ 
+    BEFORE:
+    def get(self, request, *args, **kwargs):
+      snippet = self.get_object(pk)
+      serializer = SnippetSerializer(snippet)  
+      return Response(serializer.data) 
+  """
+  """ AFTER """
+  def get(self, request, *args, **kwargs):
+    return self.retrieve(request, *args, **kwargs)
 
-    
-    return Response(serializer.data)
+  """
+    BEFORE
+    def put(self, request, *args, **kwargs):
+      snippet = self.get_object(pk)
+      serializer = SnippetSerializer(snippet, data=request.data)
 
-  def put(self, request, pk, format=None):
-    snippet = self.get_object(pk)
-    serializer = SnippetSerializer(snippet, data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  """
+  """ AFTER """
+  def put(self, request, *args, **kwargs):
+    return self.update(request, *args, **kwargs)
 
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  """ 
+    BEFORE
+    def delete(self, request, *args, **kwargs):
+      snippet = self.get_object(pk)
+      snippet.delete()
+      return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+  """ 
+  """ AFTER """
+  def delete(self, request, *args, **kwargs):
+    return self.destroy(request, *args, **kwargs)
 
-  def delete(self, request, pk, format=None):
-    snippet = self.get_object(pk)
-    snippet.delete()
-    return HttpResponse(status=status.HTTP_204_NO_CONTENT)
-    
+  
   """ 
   https://www.django-rest-framework.org/tutorial/3-class-based-views/
 
