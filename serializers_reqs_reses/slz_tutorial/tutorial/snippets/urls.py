a62a4e5bcd902d449378d620d118ad6922e38de5
Greg <gsc229@gmail.com>
2020-05-19 23:37:32 -0400

    finished requests and responses section

--- serializers_reqs_reses/slz_tutorial/tutorial/snippets/urls.py
+++ serializers_reqs_reses/slz_tutorial/tutorial/snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+## What's this? ^^^^^^^  -> https://www.django-rest-framework.org/tutorial/2-requests-and-responses/
