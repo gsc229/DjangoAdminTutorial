34e65340f41d3f060e5a7369ced5369b788a8927
Greg <gsc229@gmail.com>
2020-05-20 03:35:03 -0400

    added url paths to snipppets.urls for users and users/single, associated the corresponding views

--- serializers_reqs_reses/tutorial/snippets/views.py
+++ serializers_reqs_reses/tutorial/snippets/views.py
@@ -2,17 +2,27 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
-
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
 """
 The root of our API is going to be a view that supports listing all the existing snippets, or creating a new snippet.
 """
 
+""" GET all, POST   (generics.ListCreateAPIView) """
 class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
-  
+""" GET single, PUT, DELETE    (RetrieveUpdateDestroyAPIView) """
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
   
+"""  GET the a list of all users (ListAPIView) """
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
+"""  GET single user (RetrieveAPIView) """
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
