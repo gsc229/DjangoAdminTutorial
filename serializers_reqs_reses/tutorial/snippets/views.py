51ba9dde8a3e763ef49adf1383424e3c6727a537
Greg <gsc229@gmail.com>
2020-05-20 02:53:09 -0400

    starting auth and permissions

--- serializers_reqs_reses/tutorial/snippets/views.py
+++ serializers_reqs_reses/tutorial/snippets/views.py
@@ -6,17 +6,11 @@ from rest_framework import generics
 """
 The root of our API is going to be a view that supports listing all the existing snippets, or creating a new snippet.
 """
-"""  
-helpful refresher on *args *kwargs:
-https://realpython.com/python-kwargs-and-args/
-"""
 
 class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
   
-
-
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
